73148219ca995f48b4da99c80713ee857db37720
Julien 'Lta' BALLET <contact@lta.io>
2024-05-27 17:16:07 +0200

    fixup

--- runpod/serverless/modules/rp_fastapi.py
+++ runpod/serverless/modules/rp_fastapi.py
@@ -256,7 +256,7 @@ class WorkerAPI:
 
     def start_uvicorn(
         self, api_host='localhost', api_port=8000, api_concurrency=1,
-        reload=False, reload_dirs=[]
+        api_reload=False, api_reload_dirs=[]
     ):
         '''
         Starts the Uvicorn server.
@@ -266,7 +266,7 @@ class WorkerAPI:
             port=int(api_port), workers=int(api_concurrency),
             log_level=os.environ.get("UVICORN_LOG_LEVEL", "info"),
             access_log=False,
-            reload=reload, reload_dirs=reload_dirs
+            reload=api_reload, reload_dirs=api_reload_dirs
         )
 
     # ----------------------------- Realtime Endpoint ---------------------------- #
